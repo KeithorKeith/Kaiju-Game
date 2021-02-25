fc7349f752a818d02129c291da713baea774d33b
Stuart <stu@wigwamholidays.com>
2021-02-25 14:30:52 +0000

    Added turn counter

--- Kaiju Game/Assets/Scripts/Terminals/MainTerminalUI.cs
+++ Kaiju Game/Assets/Scripts/Terminals/MainTerminalUI.cs
@@ -1,28 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainTerminalUI : MonoBehaviour
 {
-
+    // Prefabs
     public GameObject messagePrefab;
+
+    // References to UI Components
     public GameObject messageList;
+    public Text titleText;
+
+    // Variables
+    private int currentTurn = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        AddMessage();
-        AddMessage();
+        AddMessage("Game started");
+        AddMessage("Start turn 1");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NextTurn()
     {
-        
+        currentTurn++;
+        titleText.text = $"Report Turn {currentTurn}";
+        AddMessage($"Started turn {currentTurn}");
+        AddMessage("Nothing happened...");
     }
 
-    void AddMessage()
+    void AddMessage(string messageText)
     {
-        Instantiate(messagePrefab, messageList.transform);
+        GameObject newMessage = Instantiate(messagePrefab, messageList.transform);
+        Text newText = newMessage.GetComponentInChildren<Text>();
+        if (newText)
+        {
+            newText.text = messageText;
+        }
     }
 }
